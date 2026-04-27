0af991a95c50e6ba86d76a501df8092b9f276ad9
NamNguyenee2 <nam.nguyentran.ee2@gmail.com>
2026-04-27 17:36:59 -0400

    basic programming of class and object

--- DSA/Fundamentals/basics_programming.py
+++ DSA/Fundamentals/basics_programming.py
@@ -11,3 +11,56 @@
 
 
 ## Classes and objects
+# # Approach 1
+'''
+class Car:
+
+    def __init__(self, model, year, color):
+        self.model = model
+        self.year = year
+        self.color = color
+
+    def display_information(self):
+        input("Press Enter to display car information...")
+        print(f"Car Model: {self.model}")
+        print(f"Year: {self.year}")
+        print(f"Color: {self.color}")
+
+if __name__ == "__main__":
+    
+    car1 = Car("Toyota Camry", 2020, "Black")
+    car2 = Car("Ford Escape", 2014, "Red")
+
+    car1.display_information()
+    car2.display_information()
+'''
+
+# Approach 2
+class Car:
+    def __init__(self):
+        self.model = ""
+        self.year = ""
+        self.color = ""
+
+    def display_information(self):
+        input("Press Enter to display car information...")
+        print(f"Car Model: {self.model}")
+        print(f"Year: {self.year}")
+        print(f"Color: {self.color}")
+
+if __name__ == "__main__":
+
+    car1 = Car()
+    car1.model = "Toyota Camry"
+    car1.year = 2020
+    car1.color = "Black"
+
+    car2 = Car()
+    car2.model = "Ford Escape"
+    car2.year = 2014
+    car2.color = "Red"
+
+    print("Car 1 Information:")
+    car1.display_information()
+    print("\nCar 2 Information:")
+    car2.display_information()
