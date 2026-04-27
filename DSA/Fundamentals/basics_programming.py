1809fcee67e6efd77c329c0622b0c74690d7a2b9
NamNguyenee2 <nam.nguyentran.ee2@gmail.com>
2026-04-27 17:21:26 -0400

    initialize DSA course

---
+++ DSA/Fundamentals/basics_programming.py
@@ -0,0 +1,13 @@
+# # values of a quadrilateral
+
+# a, b, c, d = input("Enter the values of four sides of a quadrilateral (space-separated for each value): ").split()
+
+# a = float(a)
+# b = float(b)
+# c = float(c)
+# d = float(d)
+
+# print("Imported values: ", a, b, c, d)
+
+
+## Classes and objects
