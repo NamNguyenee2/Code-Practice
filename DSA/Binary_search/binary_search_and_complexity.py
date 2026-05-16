a986635b57e79c66785f4d6af4cf5562119e447b
NamNguyenee2 <nam.nguyentran.ee2@gmail.com>
2026-05-15 23:01:57 -0400

    test code from website

---
+++ DSA/Binary_search/binary_search_and_complexity.py
@@ -0,0 +1,27 @@
+# source: https://jovian.com/learn/data-structures-and-algorithms-in-python/lesson/lesson-1-binary-search-and-complexity-analysis
+
+import math
+
+# print(math.sqrt(49))
+
+
+def locate_card(cards, query):
+    pass
+
+# cards = [13, 11, 10, 7, 4, 3, 1, 0]
+# query = 7
+# output = 3
+
+# We can also create a dictionary variable for the test case:
+
+test = {
+    'input': {
+        'cards': [13, 11, 10, 7, 4, 3, 1, 0],
+        'query': 7
+    },
+    'output': 3
+}
+
+# Use the following code to load:
+print(test['input'])
+# print(locate_card(**test['input']) == test['output'])
